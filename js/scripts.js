77a01d9b79bbe3f840d3c9f754db9e1b1124739f
OneWaterboy <abrahmrollins@gmail.com>
2021-02-09 20:10:09 -0800

    Finished Auto Header
    Fixed header sizing and positioning on smaller screens. Made sure that text size was appropriate and legible

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ let dateObj = new Date();
 let currentDay = dateObj.getDay();
 
 const dayOfWeek = [
-    [0, "Sorry, Sunday's We Are Closed ~ Come Food With Us Next Week!"],
-    [1, "310 New Bern Ave #174, Raleigh, NC 27601"],
-    [2, "316 Fayetteville St, Raleigh, NC 27601"],
-    [3, "350 E Six Forks Rd #150, Raleigh, NC 27609"],
-    [4, "236 Sunnybrook Rd, Raleigh, NC 27610"],
-    [5, "4020 Carya Drive Raleigh, NC 27610"],
-    [6, "1030 Merrimon Ave, Asheville, NC 28804"]
+    [0, "Sorry, Sunday's We Are Closed ~", "Come Food With Us Next Week!"],
+    [1, "310 New Bern Ave #174, ", "Raleigh, NC 27601"],
+    [2, "316 Fayetteville St, ", "Raleigh, NC 27601"],
+    [3, "350 E Six Forks Rd #150, " , "Raleigh, NC 27609"],
+    [4, "236 Sunnybrook Rd, ", "Raleigh, NC 27610"],
+    [5, "4020 Carya Drive " , "Raleigh, NC 27610"],
+    [6, "1030 Merrimon Ave, ", "Asheville, NC 28804"]
 ];
 
 
@@ -18,6 +18,7 @@ function todaysLocation () {
     for (let i=0; i < dayOfWeek.length; i++){
         if (dayOfWeek[i][0] == currentDay){
             document.getElementById('truckAddress').innerHTML = dayOfWeek[i][1];
+            document.getElementById('truckAddress2').innerHTML = dayOfWeek[i][2];
         } 
     }
 };
